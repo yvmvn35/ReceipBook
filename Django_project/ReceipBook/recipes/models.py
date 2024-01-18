44f1bccba790c2634b4815a11e64294fe0f76dbd
tojamvti <mateuszszygulski@gmail.com>
2024-01-18 22:12:00 +0100

    new

---
+++ Django_project/ReceipBook/recipes/models.py
@@ -0,0 +1,10 @@
+# recipes/models.py
+from django.db import models
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.title
